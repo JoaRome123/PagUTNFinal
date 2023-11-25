4a8ca5a72e1a67b7929881056a76584c1650a977
unknown <joakox02@gmail.com>
2023-11-25 10:07:53 -0300

    agregue los html

---
+++ acordeon.js
@@ -0,0 +1,21 @@
+function acordeon( selector ){/* Selecciona todos los elementos que coinciden con el nombre del elemento especificado */
+    const elemento = document.querySelector( selector );
+    if( ! elemento ) return;
+  
+    const items = elemento.querySelectorAll( 'li' );
+    if( items.length == 0 ) return;
+  
+   /* elemento.style.setProperty( '--items-qtty', items.length - 1 );  inactivo */
+    Array.from( items ).forEach( item => { /* Array, recorre los items del acordeon y los hace clickeables  */
+      item.addEventListener('click', e => { 
+        const anterior = elemento.querySelector( '.open' ); /* selecciona el item con la clase open y lo asigna a la const "anterior" */
+        if( anterior ) anterior.classList.remove( 'open' ); /* Si se tiene un "anterior", le quita la clase "open" */
+  
+        item.classList.add( 'open' ); /* le agrega la clase open al item clickeado para que muestre su info */
+      } );
+    } );
+    console.log( elemento ); /* console log para ver el elemento al seleccionarlo */
+  }
+  
+  acordeon( '.acordeon' ); /* Para apuntar el query selector a la clase acordeon */
+    
